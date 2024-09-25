42777369afa00b549c1bc373f0bddc91462206ae
Guilherme Silveira <61476577+GuilhermeRS11@users.noreply.github.com>
2024-09-25 20:22:08 -0300

    [PCB] - First commit with new version using Altium

--- RDM-DMX-Master-C031K6/Core/Inc/main.h
+++ RDM-DMX-Master-C031K6/Core/Inc/main.h
@@ -57,6 +57,34 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define USB_TX_Pin GPIO_PIN_2
+#define USB_TX_GPIO_Port GPIOA
+#define USB_RX_Pin GPIO_PIN_3
+#define USB_RX_GPIO_Port GPIOA
+#define LED_DMX_Received_Pin GPIO_PIN_5
+#define LED_DMX_Received_GPIO_Port GPIOA
+#define LED_DMX_Sent_Pin GPIO_PIN_6
+#define LED_DMX_Sent_GPIO_Port GPIOA
+#define Break_detection_Pin GPIO_PIN_0
+#define Break_detection_GPIO_Port GPIOB
+#define DMX_TX_Pin GPIO_PIN_9
+#define DMX_TX_GPIO_Port GPIOA
+#define DMX_RX_Pin GPIO_PIN_10
+#define DMX_RX_GPIO_Port GPIOA
+#define DMX_DE_Pin GPIO_PIN_12
+#define DMX_DE_GPIO_Port GPIOA
+#define Enter_button_Pin GPIO_PIN_15
+#define Enter_button_GPIO_Port GPIOA
+#define Up_button_Pin GPIO_PIN_3
+#define Up_button_GPIO_Port GPIOB
+#define Down_button_Pin GPIO_PIN_4
+#define Down_button_GPIO_Port GPIOB
+#define Back_button_Pin GPIO_PIN_5
+#define Back_button_GPIO_Port GPIOB
+#define Display_SCL_Pin GPIO_PIN_6
+#define Display_SCL_GPIO_Port GPIOB
+#define Display_SDA_Pin GPIO_PIN_7
+#define Display_SDA_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
